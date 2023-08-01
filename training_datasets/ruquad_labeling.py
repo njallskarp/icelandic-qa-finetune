0aadafc78ae5c1334feb8d1660c5230fbeac784d
Lsig <logis21@ru.is>
2023-08-01 11:53:12 +0000

    changed answers to be the whole paragraph, unstanderdized

--- training_datasets/ruquad_labeling.py
+++ training_datasets/ruquad_labeling.py
@@ -17,31 +17,9 @@ def get_data():
     for project_id in PROJECT_IDS:
         url = f"https://labeling.gameqa.app/api/projects/{project_id}/export?exportType=JSON"
         # Define the URL and headers
-        headers = {
-            "Accept": "*/*",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Referer": "https://labeling.gameqa.app/projects/5/data/export",
-            "Sec-Ch-Ua": '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-            "Sec-Ch-Ua-Mobile": "?0",
-            "Sec-Ch-Ua-Platform": "macOS",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "same-origin",
-            "Sec-Fetch-Site": "same-origin",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
-        }
-
-        # Define the cookies
-        cookies = {
-            "i18n_redirected": "en", 
-            "_ga_2X09492CEB": "GS1.1.1685019468.27.1.1685019468.0.0.0", 
-            "_ga": "GA1.2.1330172852.1682087891", 
-            "csrftoken": "vFXEabLncincpaRN9yCS6o8l3qovl4cTvL6ITC2JFMLFCl8eeWklKG6GHPNlqdZA", 
-            "sessionid": ".eJxVj0tuxCAQRO_CeowaGgx4mX3OYDUf22QsGBlbyke5e-xoNrOsrqpX6h925MgGppSUIVjdEQnbKdn7zgqSHU6QJHjvXNTsxlZq-7jWORc2iN7qXrgeDUfAs6JvbKRjX8ajpW38xwr2cvMU7qlcRvygMlceatm37PkV4U-38fca0_r2zL4AFmrL2UabcBI-gUa0RnoQMClnlJ-oFxA0CtIqAJK2FBUmD9pEkKTRKEfCndC6zVTyN-25lvFxP985h1pq7dLp85G3LzYolADw-wfOXFo4:1q7CV6:yrVw6xYWNwLTwoXfbO8rCTkWJM9eOQ0o7bdUX0f7gzY"
-        }
-
+        headers = {"Authorization": f"Token {api_key}"}
         # Make the GET request
-        response = requests.get(url, headers=headers, cookies=cookies)
+        response = requests.get(url, headers=headers)
 
         
         # Print the response
@@ -58,9 +36,10 @@ def get_data():
             label = annotation['labels'][0]
             if label == "Archive":
                 continue
-            start_idx = annotation['start']
-            end_idx = annotation['end']
             p = record['meta']['paragraph']
+
+            start_idx = 0
+            end_idx = len(p)
             
             answer_key = (p, start_idx, end_idx)
             q = record['meta']['question']
